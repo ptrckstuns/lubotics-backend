d481cbe66fe2e45a6e5cde65b45e1d6fbf5e56c6
ptrckstuns <ptrck.stuns@gmail.com>
2021-04-07 14:09:22 +0800

    Fourth commit: Major changes

--- blog/views.py
+++ blog/views.py
@@ -4,23 +4,6 @@ from .models import Post
 
 # Create your views here.
 
-posts = [
-	{
-		'author': 'Patrick Castro',
-		'title': 'Blog Post 1',
-		'content': 'First post content',
-		'date_posted': 'April 1, 2021',
-	},
-
-	{
-		'author': 'Chaewon',
-		'title': 'Blog Post 2',
-		'content': 'Second post content',
-		'date_posted': 'April 2, 2021',
-	}
-
-]
-
 def home(request):
 	context = {
 		'posts': Post.objects.all()

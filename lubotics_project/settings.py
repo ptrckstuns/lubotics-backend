af716bbf16d8398861731602d95eb63ebbd6a255
ptrckstuns <ptrck.stuns@gmail.com>
2021-05-28 12:08:38 +0800

    Update settings.py

--- lubotics_project/settings.py
+++ lubotics_project/settings.py
@@ -20,12 +20,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'nc@p!apf)=ea@^-z9bkt6h!-tum$s!#u-eu5!1u!sfc&d_ui2t'
+# SECRET_KEY = 'nc@p!apf)=ea@^-z9bkt6h!-tum$s!#u-eu5!1u!sfc&d_ui2t'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['lubotics.herokuapp.com']
 
 
 # Application definition

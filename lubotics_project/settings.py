6ee5520cb4066ed7ed3eda081da7dc56dfa2ade9
ptrckstuns <ptrck.stuns@gmail.com>
2021-05-29 01:18:19 +0800

    Update settings.py

--- lubotics_project/settings.py
+++ lubotics_project/settings.py
@@ -21,8 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'nc@p!apf)=ea@^-z9bkt6h!-tum$s!#u-eu5!1u!sfc&d_ui2t'
-# SECRET_KEY = os.environ.get('SECRET_KEY')
+# SECRET_KEY = 'nc@p!apf)=ea@^-z9bkt6h!-tum$s!#u-eu5!1u!sfc&d_ui2t'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
